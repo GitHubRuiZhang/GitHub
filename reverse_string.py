4c702a0ec58434a1c42f4557d1bf2d8716a645e8
GitHubRuiZhang <zhangrui55303@gmail.com>
2017-09-25 18:38:54 -0400

    Add files via upload

---
+++ reverse_string.py
@@ -0,0 +1,16 @@
+# python3
+# Write a function that takes a string as input and returns the string reversed.
+
+# Example:
+# Input: s = 'hello'
+# Output: olleh
+
+# My solution
+# it is simple to solve this problem with python
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s[::-1]
