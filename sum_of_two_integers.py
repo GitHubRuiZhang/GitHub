b8432034146bf15dca046bab742e7226b682096a
GitHubRuiZhang <zhangrui55303@gmail.com>
2017-10-09 19:09:48 -0400

    Add files via upload

---
+++ sum_of_two_integers.py
@@ -0,0 +1,8 @@
+# python3
+# Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+# Example:
+# Given a = 1 and b = 2, return 3.
+
+
+# Solution, I'm not exactly understand this.
