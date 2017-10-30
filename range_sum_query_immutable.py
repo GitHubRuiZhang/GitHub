94adb6fd3b155edf7228cbb4c1f6cd49989aaa50
GitHubRuiZhang <zhangrui55303@gmail.com>
2017-10-30 12:55:48 -0400

    Add files via upload

---
+++ range_sum_query_immutable.py
@@ -0,0 +1,63 @@
+# python3
+# Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+
+# Example:
+# Given nums = [-2, 0, 3, -5, 2, -1]
+
+# sumRange(0, 2) -> 1
+# sumRange(2, 5) -> -1
+# sumRange(0, 5) -> -3
+# Note:
+# You may assume that the array does not change.
+# There are many calls to sumRange function.
+
+
+# My solution I
+class NumArray(object):
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.nums = nums
+        self.sum = 0
+
+    def sumRange(self, i, j):
+        """
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        return sum(self.nums[i:j + 1])
+
+
+
+
+        # Your NumArray object will be instantiated and called as such:
+        # obj = NumArray(nums)
+        # param_1 = obj.sumRange(i,j)
+
+
+# My solution II
+class NumArray(object):
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.accu = [0]
+        for i in range(0, len(nums)):
+            self.accu.append(self.accu[i] + nums[i])
+
+    def sumRange(self, i, j):
+        """
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        return self.accu[j + 1] - self.accu[i]
+
+
+
+
+        # Your NumArray object will be instantiated and called as such:
+        # obj = NumArray(nums)
+        # param_1 = obj.sumRange(i,j)
