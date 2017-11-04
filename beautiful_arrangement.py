38be53ebd69ae2d22e7c0cea2cdd37ccb4f3220b
GitHubRuiZhang <zhangrui55303@gmail.com>
2017-11-04 15:51:36 -0400

    Add files via upload

---
+++ beautiful_arrangement.py
@@ -0,0 +1,49 @@
+# python3
+# Suppose you have N integers from 1 to N. We define a beautiful arrangement as an array that is constructed
+# by these N numbers successfully if one of the following is true for the ith position (1 <= i <= N) in this array:
+
+# The number at the ith position is divisible by i.
+# i is divisible by the number at the ith position.
+# Now given N, how many beautiful arrangements can you construct?
+
+# Example 1:
+# Input: 2
+# Output: 2
+# Explanation:
+
+# The first beautiful arrangement is [1, 2]:
+
+# Number at the 1st position (i=1) is 1, and 1 is divisible by i (i=1).
+
+# Number at the 2nd position (i=2) is 2, and 2 is divisible by i (i=2).
+
+# The second beautiful arrangement is [2, 1]:
+
+# Number at the 1st position (i=1) is 2, and 2 is divisible by i (i=1).
+
+# Number at the 2nd position (i=2) is 1, and i (i=2) is divisible by 1.
+# Note:
+# N is a positive integer and will not exceed 15.
+
+
+# My solution
+class Solution(object):
+    def countArrangement(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+
+        def count(n, nums):
+            if n == 1:
+                return 1
+            out = 0
+            for num in nums:
+                if num % n == 0 or n % num == 0:
+                    out += count(n - 1, nums - set([num]))
+
+            return out
+
+        nums = set([i + 1 for i in range(N)])
+        return count(N, nums)
+
